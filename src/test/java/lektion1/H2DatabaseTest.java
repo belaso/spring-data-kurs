8e7fc77ce519950784793d7239ed94c5a7bb38a4
Christian Trutz <info@christian-trutz.de>
2018-10-28 05:29:43 +0100

    H2 Datenbank Test erstellt.

---
+++ src/test/java/lektion1/H2DatabaseTest.java
@@ -0,0 +1,27 @@
+package lektion1;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.h2.tools.Server;
+import org.junit.jupiter.api.Test;
+
+public class H2DatabaseTest {
+
+	@Test
+	public void testStartDatabaseInMemory() throws SQLException, IOException {
+		JdbcDataSource dataSource = new JdbcDataSource();
+		dataSource.setUrl("jdbc:h2:mem:testdb");
+		dataSource.setUser("sa");
+		dataSource.setPassword("");
+
+		Connection connection = dataSource.getConnection();
+		connection.createStatement().execute("create table person(nachname varchar(100))");
+		connection.createStatement().executeUpdate("insert into person(nachname) values('Trutz')");
+		Server.startWebServer(connection);
+		connection.close();
+	}
+
+}
